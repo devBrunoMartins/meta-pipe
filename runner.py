dbfa9f4c94bfbb12e65e12580818e591409ce7cb
Bruno Martins <bruno.eletr@gmail.com>
2026-05-26 14:39:35 +0000

    feat: adiciona runner principal da pipeline

---
+++ runner.py
@@ -0,0 +1,23 @@
+from bronze.run_bronze import run as run_bronze
+from silver.run_silver import run as run_silver
+from gold.run_gold import run as run_gold
+
+
+def pipe_run():
+    # Executa camada bronze
+    print(f"\n\033[33mExtract\033[0m")
+    run_bronze()
+
+    # Executa camada silver
+    print(f"\n\033[33mTransform\033[0m")
+    run_silver()
+
+    # Executa a camada Gold
+    print(f"\n\033[33mMetrics\033[0m")
+    run_gold()
+    
+    print(f"\n\033[32mPronto!\033[0m\n")
+
+
+if __name__ == '__main__':
+    pipe_run()
