61181d78119b0f9ca6cd405a8caeeafbc2288a86
Bruno Martins <bruno.eletr@gmail.com>
2026-05-27 19:42:03 +0000

    refactor: remove retorno da função e acrescenta raise para erro de gravação

--- src/core/io/save_json.py
+++ src/core/io/save_json.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 import json
 
-def save_json(data: list, path: Path) -> bool:
+def save_json(data: list, path: Path) -> None:
     with open(path, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
-    if Path(path).exists() and Path(path).is_file:
-        return True
-
-    return False
+    if not (Path(path).exists() and Path(path).is_file):
+        raise
