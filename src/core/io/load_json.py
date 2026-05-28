f4b5f192f6fef2e9716860fe61433608f14f1560
Bruno Martins <bruno.eletr@gmail.com>
2026-05-28 03:09:32 +0000

    refactor: corrige nome da função load_joson para load_json

--- src/core/io/load_json.py
+++ src/core/io/load_json.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 
-def load_joson(path: Path) -> json:
+def load_json(path: Path) -> json:
 
     with open(path, 'r', encoding='utf-8') as file:
         return json.load(file)
