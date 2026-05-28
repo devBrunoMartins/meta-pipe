868dd390ad785ced402e77d63041ab815c9672fc
Bruno Martins <bruno.eletr@gmail.com>
2026-05-28 03:00:24 +0000

    refactor: corrige configuração dos datasets

--- src/config/datasets/pib.py
+++ src/config/datasets/pib.py
@@ -1,11 +1,10 @@
 PIB_CONFIG = {
     'label': 'PIB',
     'name': 'pib',
-    'url': ('https://apisidra.ibge.gov.br/values/'
-    't/5938/n6/all/v/37/p/2021'),
+    'url': 'https://apisidra.ibge.gov.br/values/t/5938/n6/all/v/37/p/2021',
 
-    'normalize': {
-        'errors': 'ignore'
+    'payload': {
+        'type': 'flat'
     },
 
     'pre_processing': {
@@ -13,66 +12,26 @@ PIB_CONFIG = {
     },
 
     'columns': [
-
-        {
-            'source': ['D1C'],
-            'target': 'city_id'
-        },
-
-        {
-            'source': ['D3C'],
-            'target': 'year'
-        },
-
-        {
-            'source': ['V'],
-            'target': 'pib'
-        },
-
-        {
-            'source': ['D1N'],
-            'target': 'city_name'
-        },
-
-        {
-            'source': ['MN'],
-            'target': 'unit'
-        },
-
-        {
-            'source': ['D2N'],
-            'target': 'variable_name'
-        }
+        {'source': ['D1C'], 'target': 'city_id'},
+        {'source': ['D3C'], 'target': 'year'},
+        {'source': ['V'], 'target': 'pib'},
+        {'source': ['D1N'], 'target': 'city_name'},
+        {'source': ['MN'], 'target': 'unit'},
+        {'source': ['D2N'], 'target': 'variable_name'}
     ],
 
     'dtypes': {
-
         'city_id': 'Int64',
-
         'year': 'Int64',
-
         'pib': 'Float64',
-
         'city_name': 'string',
-
         'unit': 'string',
-
         'variable_name': 'string'
     },
 
     'constraints': {
-
-        'not_null': [
-            'city_id',
-            'year',
-            'pib'
-        ],
-
+        'not_null': ['city_id', 'year', 'pib'],
         'unique': [],
-
-        'primary_key': [
-            'city_id',
-            'year'
-        ]
+        'primary_key': ['city_id', 'year']
     }
 }
--- src/config/datasets/populacao.py
+++ src/config/datasets/populacao.py
@@ -1,50 +1,24 @@
 POPULACAO_CONFIG = {
     'label': 'População',
     'name': 'populacao',
-    'url': (
-        'https://servicodados.ibge.gov.br/api/v3/'
-        'agregados/4714/periodos/2022/'
-        'variaveis/93?localidades=N6'
-    ),
+    'url': 'https://servicodados.ibge.gov.br/api/v3/agregados/4714/periodos/2022/variaveis/93?localidades=N6',
 
-    'normalize': {
-
-        'record_path': ['resultados', 'series'],
-
-        'meta': [
-            ['id'],
-            ['variavel'],
-            ['unidade']
-        ],
-
-        'errors': 'ignore',
-        'sep': '.'
+    'payload': {
+        'type': 'json_normalize',
+        'record_path': ['resultados', 'series']
     },
-
-    'columns': [
-
-        {
-            'source': ['localidade', 'id'],
-            'target': 'city_id'
-        },
-
-        {
-            'source': ['localidade', 'nome'],
-            'target': 'city_name'
-        },
-
-        {
-            'source': ['serie', '2022'],
-            'target': 'population'
-        }
-    ],
 
     'static_columns': {
         'year': 2022
     },
 
-    'dtypes': {
+    'columns': [
+        {'source': ['localidade', 'id'], 'target': 'city_id'},
+        {'source': ['localidade', 'nome'], 'target': 'city_name'},
+        {'source': ['serie', '2022'], 'target': 'population'}
+    ],
 
+    'dtypes': {
         'city_id': 'Int64',
         'city_name': 'string',
         'population': 'Int64',
@@ -52,19 +26,8 @@ POPULACAO_CONFIG = {
     },
 
     'constraints': {
-
-        'not_null': [
-            'city_id',
-            'city_name',
-            'population',
-            'year'
-        ],
-
+        'not_null': ['city_id', 'city_name', 'population', 'year'],
         'unique': [],
-
-        'primary_key': [
-            'city_id',
-            'year'
-        ]
+        'primary_key': ['city_id', 'year']
     }
 }
